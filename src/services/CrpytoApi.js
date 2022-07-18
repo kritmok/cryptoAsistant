9716b6e75e10d388dd6f9e541a873729070aef7b
Krit <krit@rareelements.io>
2022-07-18 18:35:11 +0800

    fix the chart and crypto detail page

--- src/services/CrpytoApi.js
+++ src/services/CrpytoApi.js
@@ -20,8 +20,9 @@ export const cryptoApi = createApi({
             query: (coinId) => createRequest(`/coin/${coinId}`),
         }),
         getCryptoHistory: builder.query({
-            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timePeriod=${timeperiod}`),
         }),
+        
     })
 })
 
