e4a44d07d072dfc0cf9cd521a2abf9d7dd6a2a0c
Krit <krit@rareelements.io>
2022-07-18 18:01:21 +0800

    update layout

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -41,14 +41,12 @@ function News({simplified}) {
 
       {cryptoNews?.value?.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
-          <Card hoverable className="news-card">
+          <Card hoverable className="news-card"
+          cover={<img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" className="socialMediaImage"/>}>
             <a href={news.url} target="_blank" rel="noreferrer">
-              <div className="news-image-container">
-                <Title className="news-title" level={4}>
+                <Title className="news-title" level={5}>
                   {news.name}
                 </Title>
-                <img style={{ maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news"/>
-              </div>
               <p>
                 {news.description > 100 ? `${news.description.substring(0, 100)}...` :
                 news.description}
