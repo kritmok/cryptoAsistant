0a3a98451a7c75ffcbf8b7611a639823785979fe
Krit <krit@rareelements.io>
2022-07-18 17:23:41 +0800

    modify crypto Overview search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react'
 import {Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
@@ -44,10 +44,7 @@ const Navbar = () => {
                 <Link to="/">Home</Link>
             </Menu.Item>
             <Menu.Item icon={<FundOutlined />}>
-                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-            </Menu.Item>
-            <Menu.Item icon={<MoneyCollectOutlined />}>
-                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/cryptocurrencies">Ranking</Link>
             </Menu.Item>
             <Menu.Item icon={<BulbOutlined />}>
                 <Link to="/news">News</Link>
